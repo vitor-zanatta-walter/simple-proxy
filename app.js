97310b6292fe9de40b0a1f1da3961e26a3489e44
Vitor Zanatta Walter <vitorzanattawalter@gmail.com>
2025-10-06 22:33:13 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+import express from "express";
+import { createProxyMiddleware } from "http-proxy-middleware";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+const API_ADDRESS = process.env.API_ADDRESS;
+const FRONTEND_ADDRESS = process.env.FRONTEND_ADDRESS;
+
+app.use((req, res, next) => {
+
+    if (req.path.startsWith("/api")) {
+        console.log(`[Proxy] ${req.method} ${req.url} -> ${API_ADDRESS}${req.url}`);
+        createProxyMiddleware({
+            target: API_ADDRESS,
+            changeOrigin: true,
+        })(req, res, next);
+    } else {
+        console.log(`[Proxy] ${req.method} ${req.url} -> ${FRONTEND_ADDRESS}${req.url}`);
+        createProxyMiddleware({
+            target: FRONTEND_ADDRESS,
+            changeOrigin: true,
+        })(req, res, next);
+    }
+});
+
+app.listen(8080, () => {
+    console.log("Proxy rodando na porta 8080");
+});
